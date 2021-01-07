670bf91c82efdd438b2563ea92e7967d5e4a24de
phpit3 <phamvhphuc@gmail.com>
2021-01-07 14:15:42 +0700

    socket

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-const app = express();
+const cors = require('cors')
+
+const app = express(); 
+
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
+
+app.use(cors());
 
 const users = {};
 
