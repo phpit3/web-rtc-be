5e302b8e42cf3b915abd1bfae6703bc8d03eafb7
phpit3 <phamvhphuc@gmail.com>
2021-01-07 14:02:21 +0700

    ver

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', (req, res) => res.send('Home Page Route'));
 
 server.listen(process.env.PORT || 8000, () =>
   console.log("server is running on port 8000")
